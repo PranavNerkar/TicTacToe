3eef48c8b60639f4267cd65e07018a393c19e404
PranavNerkar <pranavnerkar321@gmail.com>
2024-03-31 20:35:47 +0530

    Front-End Updates

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Square from "./Squares/Squares";
 
-function App() {
+const renderForm = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-div">
+      <div className="move-detection">
+        <div className="left">You</div>
+        <div className="right">Opponent</div>
+      </div>
+      <div>
+        <h1 className="water-back">Tic Tac Toe</h1>
+        <div className="square-wrapper">
+          {renderForm.map((arr) =>
+            arr.map((e) => {
+              return <Square />;
+            })
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
